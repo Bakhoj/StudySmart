3483785f27c0b7786cfad0eb19fe6deb37ad4a82
anders <Anders2108@hotmail.com>
2015-11-09 14:50:42 +0100

    Anders: HWFrag shows dummy text

--- app/src/main/java/com/example/anders/studysmart/HomeWorkCalendar/HWFrag.java
+++ app/src/main/java/com/example/anders/studysmart/HomeWorkCalendar/HWFrag.java
@@ -1,43 +1,19 @@
 package com.example.anders.studysmart.HomeWorkCalendar;
 
 import android.app.ListFragment;
-import android.net.Uri;
 import android.os.Bundle;
-import android.app.Fragment;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.Toast;
 
-import com.example.anders.studysmart.R;
 
 /**
  * HomeWorkFragment (HWFrag)
  * will manage and setup the list for the homework in HWCAct
  *
- * http://stackoverflow.com/questions/6510550/android-listview-in-fragment
+ * http://www.vogella.com/tutorials/AndroidListView/article.html#listactivity
  */
 public class HWFrag extends ListFragment implements AdapterView.OnItemClickListener{
-
-    //private static final String ARG_PARAM1 = "param1";
-    //private static final String ARG_PARAM2 = "param2";
-
-   // private String mParam1;
-   // private String mParam2;
-
-    //private OnFragmentInteractionListener mListener;
-
-   /* public static HWFrag newInstance(String param1, String param2) {
-        HWFrag fragment = new HWFrag();
-       /* Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    } */
 
     public HWFrag() {
         // Required empty public constructor
