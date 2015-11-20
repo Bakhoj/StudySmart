1504317a8ea142e618fde78adec455d894025f44
anders <Anders2108@hotmail.com>
2015-11-20 21:45:14 +0100

    Anders home: playing with themes and colors

--- app/src/main/java/com/example/anders/studysmart/HomeWorkCalendar/HWFrag.java
+++ app/src/main/java/com/example/anders/studysmart/HomeWorkCalendar/HWFrag.java
@@ -16,7 +16,8 @@ import android.widget.Toast;
  * http://www.vogella.com/tutorials/AndroidListView/article.html#listactivity
  */
 public class HWFrag extends ListFragment implements AdapterView.OnItemClickListener {
-
+    String[] values;
+    HomeWork[] homeWorks;
     public HWFrag() {
         // Required empty public constructor
     }
@@ -24,30 +25,40 @@ public class HWFrag extends ListFragment implements AdapterView.OnItemClickListe
     @Override
     public void onActivityCreated(Bundle savedInstanceState){
         super.onActivityCreated(savedInstanceState);
-        String[] values = new String[] {"03-09 08:00:00\n" +
-                "NaS Session 3\n" +
-                "Belknap Harvard: 67-76\n",
-        "09-09 13:00:00\n" +
-                "NaS Session 4\n" +
-                "The Business History Review 34: 1-23\n",
-                "03-09 08:00:00\n" +
-                        "NaS Session 3\n" +
-                        "Belknap Harvard: 67-76\n",
-                "09-09 13:00:00\n" +
-                        "NaS Session 4\n" +
-                        "The Business History Review 34: 1-23\n",
-                "03-09 08:00:00\n" +
-                        "NaS Session 3\n" +
-                        "Belknap Harvard: 67-76\n",
-                "09-09 13:00:00\n" +
-                        "NaS Session 4\n" +
-                        "The Business History Review 34: 1-23\n",
-                "03-09 08:00:00\n" +
-                        "NaS Session 3\n" +
-                        "Belknap Harvard: 67-76\n",
-                "09-09 13:00:00\n" +
-                        "NaS Session 4\n" +
-                        "The Business History Review 34: 1-23\n"};
+
+        homeWorks = new HomeWork[] {
+                new HomeWork(55, 1509201300, 3, "NaS", "Belknap Harvard: 67-76", "Read 67-76 before next time"),
+                new HomeWork(56, 1509220800, 4, "NaS", "The Business History Review 34: 1-23", "Read 1-23 before next time")
+        };
+            values = new String[homeWorks.length];
+        for(int i = 0; i < homeWorks.length; i++){
+            values[i] = homeWorks[i].toListString();
+        }
+
+//        values = new String[] {"03-09 08:00:00\n" +
+//                    "NaS Session 3\n" +
+//                    "Belknap Harvard: 67-76\n",
+//                "09-09 13:00:00\n" +
+//                    "NaS Session 4\n" +
+//                    "The Business History Review 34: 1-23\n",
+//                "03-09 08:00:00\n" +
+//                        "NaS Session 3\n" +
+//                        "Belknap Harvard: 67-76\n",
+//                "09-09 13:00:00\n" +
+//                        "NaS Session 4\n" +
+//                        "The Business History Review 34: 1-23\n",
+//                "03-09 08:00:00\n" +
+//                        "NaS Session 3\n" +
+//                        "Belknap Harvard: 67-76\n",
+//                "09-09 13:00:00\n" +
+//                        "NaS Session 4\n" +
+//                        "The Business History Review 34: 1-23\n",
+//                "03-09 08:00:00\n" +
+//                        "NaS Session 3\n" +
+//                        "Belknap Harvard: 67-76\n",
+//                "09-09 13:00:00\n" +
+//                        "NaS Session 4\n" +
+//                        "The Business History Review 34: 1-23\n"};
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
                 android.R.layout.simple_list_item_1, values);
@@ -58,6 +69,6 @@ public class HWFrag extends ListFragment implements AdapterView.OnItemClickListe
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         Toast.makeText(getActivity(), "Item " + position, Toast.LENGTH_SHORT).show();
-        System.out.println("item: " + position);
+        System.out.print("item: " + position);
     }
 }
--- app/src/main/java/com/example/anders/studysmart/HomeWorkCalendar/HomeWork.java
+++ app/src/main/java/com/example/anders/studysmart/HomeWorkCalendar/HomeWork.java
@@ -4,10 +4,38 @@ package com.example.anders.studysmart.HomeWorkCalendar;
  * Created by anders on 19-11-2015.
  */
 public class HomeWork {
-    private int id, date;
-    private String title, description;
+    private int id, date, session;
+    private String title, description, shortD;
 
-    public HomeWork(){
+    public HomeWork(int id, int date, int session, String title, String description, String shortD){
+        this.id = id;
+        this.date = date;
+        this.session = session;
+        this.title = title;
+        this.description = description;
+        this.shortD = shortD;
+    }
 
+    public int getId(){
+        return this.id;
+    }
+    public int getDate(){
+        return this.date;
+    }
+    public int getSession() {
+        return this.session;
+    }
+    public String getTitle() {
+        return this.title;
+    }
+    public String getDescription() {
+        return this.description;
+    }
+    public String getShortD(){
+        return this.shortD;
+    }
+
+    public String toListString() {
+        return (title + "\n" + shortD);
     }
 }
